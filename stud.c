92517148eb8c213a48c7ba36efb2e1f3c851f9ed
sdivyakpr <indhusachithanantham@gmail.com>
2016-07-10 12:45:40 +0530

    finish

---
+++ stud.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the pairs c");
+scanf("%d",&c);
+for(d=0;;d<=2*n;d++)
+{
+b=2*n-d;
+if(c!=b)
+{
+printf("the pair is",c);
+}
+}
+getch();
+}
